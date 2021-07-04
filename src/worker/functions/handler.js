72c7867b56fc5f7a0b255b6a9961d2e2f82836a7
tissueMO <ts32worker@gmail.com>
2021-07-04 17:04:53 +0900

    タイムカード打刻関数を追加

--- src/worker/functions/handler.js
+++ src/worker/functions/handler.js
@@ -1,59 +1,138 @@
-const fs = require('fs').promises;
-const fetch = require('node-fetch');
 const { chromium } = require('playwright');
 const { timecard } = require('./timecard');
+const aws = require('aws-sdk');
+const sns = new aws.SNS();
+const ssm = new aws.SSM();
 
-module.exports.executeTimecard = async event => {
-  let result;
-  try {
-    result = await execute(timecard);
-  } catch (e) {
-    console.error(e);
-    // TODO: SNS通知
-    return;
+const dbRegion = process.env.db_region;
+const dbEndpoint = process.env.db_endpoint;
+const snsTopic = process.env.sns_topic;
+const kintaiUrl = process.env.kintai_url;
+
+const dynamodb = new aws.DynamoDB.DocumentClient(dbRegion ? {
+  region: dbRegion,
+  endpoint: dbEndpoint,
+} : {});
+
+/**
+ * 指定のユーザーとして勤怠システムにアクセスし、タイムカードを打刻します。
+ */
+module.exports.executeTimecard = async (event) => {
+  const {email, apiKey} = event;
+
+  const user = (await dynamodb.get({
+    TableName: 'users',
+    Key: { email },
+  }).promise())?.Item;
+
+  if (!user) {
+    return {
+      statusCode: 400,
+      body: `データベースに登録されていないユーザーを削除しようとしました: ${email}`,
+    };
+  }
+  if (user.api_key !== apiKey) {
+    return {
+      statusCode: 400,
+      body: `APIキーが不正です: (${email}) ${apiKey}`,
+    };
   }
 
-  // TODO: SNS通知
-  // `タイムカード [${result}] 打刻しました。`,
-  res.send(`${result}`);
+  const password = await ssm.getParameter({
+    Name: user.kintai_password_ssm_parameter_name,
+    WithDecryption: true,
+  }).promise()?.Parameter?.Value;
+
+  if (!password) {
+    return {
+      statusCode: 400,
+      body: `勤怠システムのパスワードが登録されていません: ${email}`,
+    };
+  }
+
+  const options = {
+    username: user.kintai_username,
+    password,
+  };
+
+  try {
+    const result = await executeOnKintai(timecard, options);
+
+    await sns.publish({
+      TopicArn: snsTopic,
+      Subject: `【${result}】タイムカード打刻完了`,
+      Message: `タイムカード【${result}】打刻しました。\n`,
+      MessageAttributes: {
+        to: {
+          DataType: 'String',
+          StringValue: email,
+        },
+      },
+    }).promise();
+
+    return {
+      statusCode: 200,
+      result,
+    };
+
+  } catch (e) {
+    console.error(e);
+
+    await sns.publish({
+      TopicArn: snsTopic,
+      Subject: 'タイムカード打刻失敗',
+      Message:
+        'タイムカードの打刻に失敗しました。\n\n' +
+        `◆エラー内容\n${e}\n`,
+      MessageAttributes: {
+        to: {
+          DataType: 'String',
+          StringValue: email,
+        },
+      },
+    }).promise();
+
+    return {
+      statusCode: 500,
+      error: e,
+    };
+  }
 };
 
 /**
  * 勤怠システム上で任意の処理を行います。
- *
  * @param {Function} callback
  * @param {Object} options
+ * @returns {*}
  */
-const execute = async (callback, options) => {
+const executeOnKintai = async (callback, options) => {
+  const { username, password } = options;
+
   // 勤怠システムにアクセス
   const browser = await chromium.launch();
   const context = await browser.newContext({
     locale: 'ja',
     viewport: { width: 1920, height: 1080 },
-    recordVideo: record ? {
-      dir: '.',
-      size: { width: 1920, height: 1080 },
-    } : undefined,
   });
   const page = await context.newPage();
-  await page.goto(url);
+  await page.goto(kintaiUrl);
 
   // ログイン
-  (await page.$('input[name="username"]')).fill(user);
+  (await page.$('input[name="username"]')).fill(username);
   (await page.$('input[name="password"]')).fill(password);
   await page.click('input[name="submit1"]');
 
   // 共通フレームページへ
-  await page.goto(`${url}/common/mainframe.asp`);
+  await page.goto(`${kintaiUrl}/common/mainframe.asp`);
 
-  // 任意の処理を呼び出し
+  // 任意の処理を実行
   let result = null;
   if (callback) {
     result = await callback(page, options);
   }
 
   // ログアウト
-  await page.goto(`${url}/common/logout.asp`);
+  await page.goto(`${kintaiUrl}/common/logout.asp`);
   await browser.close();
 
   return result;
--- src/worker/functions/timecard.js
+++ src/worker/functions/timecard.js
@@ -6,8 +6,8 @@ module.exports = {
    * @param {Page} page
    * @returns {string}
    */
-  timecard: async page => {
-    // タイムカードページへ
+  timecard: async (page) => {
+    // タイムカードページへ移動
     const sideFrameElement = await page.$('frame[name="oldmenuFrame"]');
     const sideFrame = await sideFrameElement.contentFrame();
     await sideFrame.click('a[href="./work/registtimeend1.asp"]');
@@ -24,19 +24,22 @@ module.exports = {
     }
 
     const command = (buttons.length === 4) ? '出勤' : '退勤';
-    if (command === '出勤') {
-      console.info('出勤タイムカードを打刻します。');
-      await buttons.slice(0, 1).pop().click();
-    } else {
-      console.info('退勤タイムカードを打刻します。');
-      await buttons.slice(-1).pop().click();
-    }
 
-    // 画面が更新されるのを待つ
-    const newBodyFrameElement = await page.$('frame[name="bodyFrame"]');
-    const newBodyFrame = await newBodyFrameElement.contentFrame();
-    await newBodyFrame.waitForLoadState('load');
-    await newBodyFrame.waitForSelector('a[id="today"]');
+    console.info(`<<< ドライラン [${command}] >>>>`);
+
+    // if (command === '出勤') {
+    //   console.info('出勤タイムカードを打刻します。');
+    //   await buttons.slice(0, 1).pop().click();
+    // } else {
+    //   console.info('退勤タイムカードを打刻します。');
+    //   await buttons.slice(-1).pop().click();
+    // }
+
+    // // 画面が更新されるのを待つ
+    // const newBodyFrameElement = await page.$('frame[name="bodyFrame"]');
+    // const newBodyFrame = await newBodyFrameElement.contentFrame();
+    // await newBodyFrame.waitForLoadState('load');
+    // await newBodyFrame.waitForSelector('a[id="today"]');
 
     return command;
   },
