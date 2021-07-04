cd2759ed621fc8db996f18fa408de4ad3eec99c7
tissueMO <ts32worker@gmail.com>
2021-07-05 01:50:07 +0900

    タイムカード打刻API完成

--- src/workers/timecard.js
+++ src/workers/timecard.js
@@ -27,21 +27,19 @@ module.exports = {
 
     const command = (buttons.length === 4) ? '出勤' : '退勤';
 
-    console.info(`<<< ドライラン [${command}] >>>>`);
+    if (command === '出勤') {
+      console.info('出勤タイムカードを打刻します。');
+      await buttons.slice(0, 1).pop().click();
+    } else {
+      console.info('退勤タイムカードを打刻します。');
+      await buttons.slice(-1).pop().click();
+    }
 
-    // if (command === '出勤') {
-    //   console.info('出勤タイムカードを打刻します。');
-    //   await buttons.slice(0, 1).pop().click();
-    // } else {
-    //   console.info('退勤タイムカードを打刻します。');
-    //   await buttons.slice(-1).pop().click();
-    // }
-
-    // // 画面が更新されるのを待つ
-    // const newBodyFrameElement = await page.$('frame[name="bodyFrame"]');
-    // const newBodyFrame = await newBodyFrameElement.contentFrame();
-    // await newBodyFrame.waitForLoadState('load');
-    // await newBodyFrame.waitForSelector('a[id="today"]');
+    // 画面が更新されるのを待つ
+    const newBodyFrameElement = await page.$('frame[name="bodyFrame"]');
+    const newBodyFrame = await newBodyFrameElement.contentFrame();
+    await newBodyFrame.waitForLoadState('load');
+    await newBodyFrame.waitForSelector('a[id="today"]');
 
     return command;
   },
